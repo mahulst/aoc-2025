f5eb88a6f09268aa8a4aee303b04e755c43a22fb
mahulst <michel@voorkanter.com>
2025-12-01 11:47:29 +0100

    Day 1

---
+++ day1/main.c
@@ -0,0 +1,8 @@
+// #include "part_1.c"
+#include "part_2.c"
+
+int main(){
+  // part_1();
+  part_2();
+
+}
---
+++ day1/part_1.c
@@ -0,0 +1,59 @@
+#include <stdint.h>
+#define ARENA_IMPLEMENTATION
+
+#include "arena.h"
+#include <stdio.h>
+
+typedef enum Dir { Left, Right } Dir;
+typedef struct {
+  enum Dir dir;
+  uint32_t amount;
+} Turn;
+
+typedef struct {
+  Turn *items;
+  size_t count;
+  size_t capacity;
+} TurnArray;
+
+void part_1() {
+  Arena arena = {0};
+  TurnArray turns = {0};
+  FILE *file;
+  file = fopen("./input.txt", "r");
+  char buffer[256];
+  while (fscanf(file, "%255[^\n]\n", buffer) != EOF) {
+    Dir dir;
+    if (buffer[0] == 'L') {
+      dir = Left;
+    } else {
+      dir = Right;
+    }
+    uint32_t amount = atoi(buffer + 1);
+    Turn t = {.dir = dir, .amount = amount};
+
+    arena_da_append(&arena, &turns, t);
+  }
+
+
+  int32_t dial = 50;
+  uint32_t counter = 0;
+  for (uint32_t i = 0, n = turns.count; i < n; i += 1) {
+    Turn t = turns.items[i];
+    if (t.dir == Left) {
+      dial -= t.amount;
+    }
+    if (t.dir == Right) {
+      dial += t.amount;
+    }
+    dial = dial % 100;
+    if (dial < 0){
+      dial += 100;
+    }
+    if (dial == 0) {
+      counter += 1;
+    }
+  }
+  printf("part 1 answer: %u", counter);
+}
+
